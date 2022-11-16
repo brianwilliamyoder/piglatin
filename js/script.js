3180e203f7a094c4ef5e9d997e5cc753f62aa5d1
Brian Yoder <brianyoder@gmail.com>
2022-11-15 17:24:40 -0800

    BUGFIX: refactor 1st test - outputs vowel with way success

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
 //1st test
-
 function pigLatin(text) {
   let textArray = text.split("");
   console.log(textArray);
-  //const vowels = ["a", "e", "i", "o", "u"];
+  const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z'];
+  const vowels = ["a", "e", "i", "o", "u"];
+  let pigTextOutput = "";
+  for (let i = 0; i < consonants.length; i += 1) {
+    if (textArray[0] === vowels[i]) {
+     pigTextOutput = textArray.join().concat("way");
+   }
+     console.log(pigTextOutput);      
+  } 
+}
+  
+//2nd Test
+function pigLatin(text) {
+  let textArray = text.split("");
   const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z'];
   let pigTextOutput = "";
-  for (let index = 0; index < consonants.length; index += 1) {
-  if (textArray[0] === consonants[index]) {
-    let slicedArray = textArray.slice(1);
-    slicedArray = slicedArray.push(textArray[0]);
-    pigTextOutput = slicedArray.join("");
-    return;
-    //let firstElement = textArray.shift();
-   // let newArray = textArray.push(firstElement);
-    //pigTextOutput = newArray.join();
-    //pigTextOutput = text.concat("way");
-  } /*else if (textArray[0] !== vowels[index]) {
-  }*/
+  for (let i = 0; i < consonants.length; i += 1) {
+    if (textArray[0] === consonants[i]) {
+      let slicedArray = textArray.slice(1);
+      slicedArray.push(textArray[0]);
+      pigTextOutput = slicedArray.join("");
+      console.log(pigTextOutput);
+      return;
+     } else {
   console.log(pigTextOutput);
-  }
-  
+     }
+   }
 }
